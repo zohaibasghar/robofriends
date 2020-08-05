7cf3a556e6a60b4b5b91b2e4ccd958c0c6982ef9
zohaibasghar <32808057+zohaibasghar@users.noreply.github.com>
2020-08-05 12:16:09 +0500

    build changes
    added my new react app build folder

---
+++ precache-manifest.626a891b9c409d3777c65c342b6e8dec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f3a9742014aaf8b55cf310bfa938d03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc0d58ead9737cd69c0e",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5dff6d4b54d2be9046f3",
+    "url": "/static/css/main.a44635d6.chunk.css"
+  },
+  {
+    "revision": "cc0d58ead9737cd69c0e",
+    "url": "/static/js/2.2a06c861.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2a06c861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dff6d4b54d2be9046f3",
+    "url": "/static/js/main.9a3c351e.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  }
+]);
